7f8b68eda26d5da549274e9431e71f91a5fe1e71
Shine <113924792+HeanShine@users.noreply.github.com>
2023-10-16 14:09:39 +0700

    them crud permission controller 16/10-14:09 bang

---
+++ WorkFlow/src/main/java/com/workflow/service/IPermissionTeamService.java
@@ -0,0 +1,4 @@
+package com.workflow.service;
+
+public interface IPermissionTeamService {
+}
--- WorkFlow/src/main/java/com/workflow/service/impl/PermissionTeamServiceImpl.java
+++ WorkFlow/src/main/java/com/workflow/service/impl/PermissionTeamServiceImpl.java
@@ -26,6 +26,7 @@ public class PermissionTeamServiceImpl implements IPermissionTeamService {
     public List<PermissionTeam> getByUsername(String username){
         return permissionTeamRepo.findAllByAccount_Username(username);
     }
+
     public void addMember(AddMemberRequest addMemberRequest){
         permissionTeamRepo.save(builderAdd(addMemberRequest));
     }
